e028cc5836769e7d92274a0e9967fea169e306ec
D. A. Jones <122707430+da314jones@users.noreply.github.com>
2024-03-15 18:03:51 -0400

    Create videoRoutes.js

---
+++ routes/videoRoutes.js
@@ -0,0 +1,19 @@
+import express from "express";
+import videoController from "../controllers/videoController.js";
+const videos = express.Router();
+
+videos.post("/session", videoController.creatingSession);
+
+videos.get("/token/:sessionId", videoController.generatingToken);
+
+videos.post("/video-metadata/", videoController.createVideoMetadata);
+
+videos.post("/start-recording", videoController.startVideoRecording);
+
+videos.post("/stop-recording", videoController.stopVideoRecording);
+
+videos.post("/upload-recording", videoController.uploadVideo);
+
+// videos.get ('/list', videoController.listFiles)
+
+export default videos;
