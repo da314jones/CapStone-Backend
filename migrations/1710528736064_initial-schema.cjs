4ed7d95c06e8a07bcaaa8bab19f5914b3c506ea1
D. A. Jones <122707430+da314jones@users.noreply.github.com>
2024-03-15 17:58:17 -0400

    Create 1710528736064_initial-schema.cjs

---
+++ migrations/1710528736064_initial-schema.cjs
@@ -0,0 +1,33 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = pgm => {
+    pgm.createTable('users', {
+      id: 'id',
+      firstName: { type: 'varchar(255)' },
+      lastName: { type: 'varchar(255)' },
+      email: { type: 'varchar(255)', notNull: true, unique: true },
+      photo_url: { type: 'varchar(255)' },
+      firebase_uid: { type: 'varchar(255)', notNull: true, unique: true },
+      created_at: { type: 'timestamp', default: pgm.func('current_timestamp') },
+    });
+
+    pgm.createTable('videos', {
+        id: 'id',
+        user_id: { type: 'integer', references: '"users"', onDelete: 'cascade' },
+        title: { type: 'varchar(255)', notNull: true },
+        summary: { type: 'text' },
+        ai_summary: { type: 'text' },
+        video_url: { type: 'varchar(255)', notNull: true },
+        is_private: { type: 'boolean' },
+        duration: { type: 'integer', notNull: true },
+        created_at: { type: 'timestamp with time zone', default: pgm.func('current_timestamp') },
+        updated_at: { type: 'timestamp with time zone', default: pgm.func('current_timestamp') }
+      });
+};
+exports.down = pgm => {
+    pgm.dropTable('videos');
+    pgm.dropTable('users');
+
+};
