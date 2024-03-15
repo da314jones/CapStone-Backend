d64ca5129ff0f5ad48c45098f8329e125c5a2be4
D. A. Jones <122707430+da314jones@users.noreply.github.com>
2024-03-15 17:55:49 -0400

    Update dbConfig.js

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,1 +1,16 @@
+import pgPromise from 'pg-promise';
 
+const pgp = pgPromise({});
+
+
+const cn = {
+    host: process.env.PG_HOST, 
+    port: process.env.PG_PORT, 
+    database: process.env.PG_DATABASE, 
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD
+};
+
+const db = pgp(cn);
+
+export { db }
