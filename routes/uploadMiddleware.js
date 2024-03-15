69af6f791b6f878edd855de578bda892dab3d544
D. A. Jones <122707430+da314jones@users.noreply.github.com>
2024-03-15 18:03:22 -0400

    Create uploadMiddleware.js

---
+++ routes/uploadMiddleware.js
@@ -0,0 +1,23 @@
+// uploadMiddleware.js
+import multer from "multer";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// Set up storage location for multer
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "../uploads"));
+    // Temporary directory for uploads
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + "-" + file.originalname);
+  },
+});
+
+// Create multer upload middleware
+const upload = multer({ storage: storage });
+
+export default upload;
