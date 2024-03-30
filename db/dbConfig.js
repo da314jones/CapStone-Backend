d0b0d8c8e20b22d3260c84ace9a2c06eb6ff88f8
D Jones <djones@Ds-MacBook-Air.mynetworksettings.com>
2024-03-29 20:48:21 -0400

    dbconfig 3rd deploy adjust

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,20 +1,18 @@
 import pgPromise from 'pg-promise';
+import dotenv from 'dotenv';
+dotenv.config();
 
-import dotenv from "dotenv"
-const pgp = pgPromise({});
-dotenv.config()
+const DATABASE_URL = process.env.DATABASE_URL; 
 
-const DATABASE_URL = process.env.DATABASE_URL
 const cn = {
-    databaseURL: process.env.DATABASE_UR,
     host: process.env.PG_HOST, 
     port: process.env.PG_PORT,
     database: process.env.PG_DATABASE,
     user: process.env.PG_USER,
     password: process.env.PG_PASSWORD
-}
+};
 
-
+const pgp = pgPromise({});
 const db = pgp(cn);
 
-export { db }
+export { db };
